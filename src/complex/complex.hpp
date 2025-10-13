5d9a4f69e26cdd977f089364706a20a31a4d7060
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-13 09:41:32 +0100

    Create setter and adder definitions

--- src/complex/complex.hpp
+++ src/complex/complex.hpp
@@ -6,4 +6,10 @@ private:
 public:
   float getReal() const;
   float getImaginary() const;
+
+  void setReal(float real);
+  void setImaginary(float imaginary);
+
+  float addReal(float real);
+  float addImaginary(float imaginary);
 };
