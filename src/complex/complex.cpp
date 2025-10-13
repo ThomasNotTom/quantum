bf5bdba57a95a0cb3b4b347b5e64699fa1be5b1d
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-13 09:50:50 +0100

    Define setter implementation

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -6,3 +6,6 @@ Complex::Complex() : real(0.0f), imaginary(0.0f) {};
 
 float Complex::getReal() const { return this->real; };
 float Complex::getImaginary() const { return this->imaginary; };
+
+void Complex::setReal(float real) { this->real = real; };
+void Complex::setImaginary(float imaginary) { this->imaginary = imaginary; };
