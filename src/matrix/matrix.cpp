b4c3cdad59bdc926c44d0ee1c449d37c61cd60ff
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-13 12:58:36 +0100

    Create get implementation

--- src/matrix/matrix.cpp
+++ src/matrix/matrix.cpp
@@ -6,3 +6,7 @@ Matrix::Matrix(unsigned int width, unsigned int height)
 Matrix::Matrix(unsigned int width, unsigned int height,
                const Complex initialValue)
     : values(height, std::vector<Complex>(width, initialValue)) {};
+
+Complex Matrix::get(unsigned int x, unsigned int y) const {
+  return this->values[y][x];
+}
