495f55b6d46f39a13ba7dc97bceca743db0755cb
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-13 10:21:56 +0100

    Create with argument test

--- tests/complex.cpp
+++ tests/complex.cpp
@@ -12,3 +12,15 @@ TEST_CASE("Complex get with no arguments to constructor", "[complex]") {
   imaginary = c.getImaginary();
   REQUIRE(imaginary == 0.0f);
 }
+
+TEST_CASE("Complex get with arguments to constructor", "[complex]") {
+  const float A = 2.0f;
+  const float B = 3.0f;
+
+  Complex c = Complex(A, B);
+  float real, imaginary;
+  real = c.getReal();
+  REQUIRE(real == A);
+  imaginary = c.getImaginary();
+  REQUIRE(imaginary == B);
+}
