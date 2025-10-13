3bb9e4a95751a6d2c7fdb97c87accfbe64e85151
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-13 13:00:51 +0100

    Create test for constructor with default value

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -7,3 +7,11 @@ TEST_CASE("Matrix get with no default value", "[matrix]") {
   Complex value = m.get(0, 0);
   REQUIRE(value == Complex(0.0f, 0.0f));
 };
+
+TEST_CASE("Matrix get with default value", "[matrix]") {
+  const Complex def = Complex(2.0f, 3.0f);
+
+  Matrix m = Matrix(1, 1, def);
+  Complex value = m.get(0, 0);
+  REQUIRE(value == def);
+};
