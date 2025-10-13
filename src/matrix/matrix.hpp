4516e5cb1e3ca158df106c04c282174ae8784b91
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-13 13:02:51 +0100

    Define constructor of values

--- src/matrix/matrix.hpp
+++ src/matrix/matrix.hpp
@@ -9,6 +9,7 @@ private:
 public:
   Matrix(unsigned int width, unsigned int height);
   Matrix(unsigned int width, unsigned int height, const Complex initialValue);
+  Matrix(const std::vector<std::vector<Complex>>& values);
 
   Complex get(unsigned int x, unsigned int y) const;
 };
