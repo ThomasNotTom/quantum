5f8090a9ed5d6921847dfe9d432224caa3b394e7
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 00:07:41 +0100

    Create get and set implementations

--- src/ket/ket.cpp
+++ src/ket/ket.cpp
@@ -6,3 +6,7 @@ Ket::Ket(unsigned int height, const Complex initialValue)
     : values(height, initialValue) {};
 
 Ket::Ket(const std::vector<Complex>& values) : values(values) {};
+
+Complex Ket::get(unsigned int y) const { return this->values[y]; };
+
+void Ket::set(unsigned int y, const Complex value) { this->values[y] = value; };
