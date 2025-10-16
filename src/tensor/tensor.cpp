f8d66ead37421566c319a6b5d4d376506da3ab91
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 17:05:34 +0100

    Remove tensor implementation file

--- src/tensor/tensor.cpp
+++
@@ -1,8 +0,0 @@
-#include "./tensor.hpp"
-
-template <typename T>
-Tensor<T>::Tensor(unsigned int size, const T initialValue)
-    : values(height, initialValue){};
-
-template <typename T>
-Tensor<T>::Tensor(const std::vector<T>& values) : values(values){};
