183235026efce481e0a59adf5d46594687e17fbe
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 17:04:19 +0100

    Create operator implementations

--- src/bra/bra.cpp
+++ src/bra/bra.cpp
@@ -22,3 +22,57 @@ Ket Bra::hermitian() const {
   Ket out = this->transpose();
   return out.conjugate();
 };
+
+Bra Bra::operator+(const Bra& other) const {
+  Bra out = Bra(this->getSize());
+
+  for (unsigned int y = 0; y < this->getSize(); y++) {
+    out.set(y, this->get(y) + other.get(y));
+  }
+
+  return out;
+}
+
+void Bra::operator+=(const Bra& other) {
+  const Bra thisTemp = *this;
+
+  for (unsigned int y = 0; y < this->getSize(); y++) {
+    this->set(y, thisTemp.get(y) + other.get(y));
+  }
+}
+
+Bra Bra::operator-(const Bra& other) const {
+  Bra out = Bra(this->getSize());
+
+  for (unsigned int y = 0; y < this->getSize(); y++) {
+    out.set(y, this->get(y) - other.get(y));
+  }
+
+  return out;
+}
+
+void Bra::operator-=(const Bra& other) {
+  const Bra thisTemp = *this;
+
+  for (unsigned int y = 0; y < this->getSize(); y++) {
+    this->set(y, thisTemp.get(y) - other.get(y));
+  }
+}
+
+bool Bra::operator==(const Bra& other) const {
+  for (unsigned int i = 0; i < this->getSize(); i++) {
+    if (this->get(i) != other.get(i)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+bool Bra::operator!=(const Bra& other) const {
+  for (unsigned int i = 0; i < this->getSize(); i++) {
+    if (this->get(i) == other.get(i)) {
+      return true;
+    }
+  }
+  return false;
+};
