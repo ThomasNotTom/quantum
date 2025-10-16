a6e50f6f92d0ffeb23cb749039c285731088140e
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 14:30:14 +0100

    Create subtract operation implementations

--- src/vector/vector.cpp
+++ src/vector/vector.cpp
@@ -15,3 +15,19 @@ void Vector::operator+=(const Vector& other) {
     this->set(i, thisCopy.get(i) + other.get(i));
   }
 };
+
+Vector Vector::operator-(const Vector& other) const {
+  Vector out = Vector(this->getRank());
+  for (unsigned int i = 0; i < this->getRank(); i++) {
+    out.set(i, this->get(i) - other.get(i));
+  }
+  return out;
+};
+
+void Vector::operator-=(const Vector& other) {
+  const Vector thisCopy = *this;
+
+  for (unsigned int i = 0; i < this->getRank(); i++) {
+    this->set(i, thisCopy.get(i) - other.get(i));
+  }
+};
