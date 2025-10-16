3e602927e92513c238a2d6ebcce206a7737f981b
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 15:31:41 +0100

    Inherit from vector

--- src/bra/bra.hpp
+++ src/bra/bra.hpp
@@ -1,22 +1,16 @@
-class Bra {
-  Bra(unsigned int height);
-  Bra(unsigned int height, const Complex initialValue);
-  Bra(const std::vector<Complex>& values);
+#include "../vector/vector.hpp
 
-  Complex get(unsigned int y) const;
-  void set(unsigned int y, const Complex value);
+class Ket;
+
+class Bra : public Vector {
+public:
+  using Vector::Vector;
 
   Complex inner(const Ket& other) const;
 
+  Ket transpose() const;
+  Ket hermitian() const;
+
   Bra multiply(const Matrix& other) const;
   void multiplyInplace(const Matrix& other);
-
-  Bra operator+(const Bra& other) const;
-  void operator+=(const Bra& other);
-
-  Bra operator-(const Bra& other) const;
-  void operator-=(const Bra& other);
-
-  bool operator==(const Bra& other) const;
-  bool operator!=(const Bra& other) const;
 };
