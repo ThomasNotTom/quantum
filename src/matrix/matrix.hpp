c5c3404e5ad8ff65ec5b87c7753465c2571f8846
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 15:32:14 +0100

    Inherit from vector

--- src/matrix/matrix.hpp
+++ src/matrix/matrix.hpp
@@ -3,18 +3,22 @@
 #include <vector>
 
 #include "../complex/complex.hpp"
+#include "../vector/vector.hpp"
 
-class Matrix {
+class Matrix : public Vector {
 private:
-  std::vector<std::vector<Complex>> values;
+  unsigned int width, height;
 
 public:
   Matrix(unsigned int width, unsigned int height);
   Matrix(unsigned int width, unsigned int height, const Complex initialValue);
-  Matrix(const std::vector<std::vector<Complex>>& values);
+  Matrix(const std::vector<Complex>& values);
+
+  unsigned int getWidth() const;
+  unsigned int getHeight() const;
 
   Complex get(unsigned int x, unsigned int y) const;
-  void set(unsigned int x, unsigned int y, const Complex value);
+  void set(unsigned int x, unsigned int y, Complex value);
 
   Matrix conjugate() const;
   void conjugateInplace();
@@ -27,13 +31,4 @@ public:
 
   Matrix multiply(const Matrix& other) const;
   void multiplyInplace(const Matrix& other);
-
-  Matrix operator+(const Matrix& other) const;
-  void operator+=(const Matrix& other);
-
-  Matrix operator-(const Matrix& other) const;
-  void operator-=(const Matrix& other);
-
-  bool operator==(const Matrix& other) const;
-  bool operator!=(const Matrix& other) const;
 };
