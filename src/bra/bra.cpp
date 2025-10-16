75c1ff8a757447d1f7ccb0c696923c4a8662223d
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 15:44:23 +0100

    Remove undefined method

--- src/bra/bra.cpp
+++ src/bra/bra.cpp
@@ -20,29 +20,3 @@ Ket Bra::hermitian() const {
   Ket out = this->transpose();
   return out.conjugate();
 };
-
-Bra Bra::multiply(const Matrix& other) const {
-  Bra out = Bra(this->getSize(), Complex(0.0f, 0.0f));
-
-  for (unsigned int y = 0; y < other.getHeight(); y++) {
-    Complex sum = Complex(0.0f, 0.0f);
-    for (unsigned int x = 0; x < other.getWidth(); x++) {
-      sum += this->get(y) * other.get(x, y);
-    }
-    out.set(y, sum);
-  }
-
-  return out;
-};
-
-void Bra::multiplyInplace(const Matrix& other) {
-  const Bra thisTemp = *this;
-
-  for (unsigned int y = 0; y < other.getHeight(); y++) {
-    Complex sum = Complex(0.0f, 0.0f);
-    for (unsigned int x = 0; x < other.getWidth(); x++) {
-      sum += thisTemp.get(y) * other.get(x, y);
-    }
-    this->set(y, sum);
-  }
-};
