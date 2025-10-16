579746d0d46f772b1556b11a7061e366c6ab5783
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 14:29:37 +0100

    Create add operation implementations

---
+++ src/vector/vector.cpp
@@ -0,0 +1,17 @@
+#include "./vector.hpp"
+
+Vector Vector::operator+(const Vector& other) const {
+  Vector out = Vector(this->getRank());
+  for (unsigned int i = 0; i < this->getRank(); i++) {
+    out.set(i, this->get(i) + other.get(i));
+  }
+  return out;
+};
+
+void Vector::operator+=(const Vector& other) {
+  const Vector thisCopy = *this;
+
+  for (unsigned int i = 0; i < this->getRank(); i++) {
+    this->set(i, thisCopy.get(i) + other.get(i));
+  }
+};
