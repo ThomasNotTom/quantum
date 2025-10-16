197fcfdea4ffdd9b385eabadc47b450c6bea9655
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 12:52:00 +0100

    Ceate bra method definitions

--- src/bra/bra.hpp
+++ src/bra/bra.hpp
@@ -1,1 +1,22 @@
-class Bra {};
+class Bra {
+  Bra(unsigned int height);
+  Bra(unsigned int height, const Complex initialValue);
+  Bra(const std::vector<Complex>& values);
+
+  Complex get(unsigned int y) const;
+  void set(unsigned int y, const Complex value);
+
+  Complex inner(const Bra& other) const;
+
+  Bra multiply(const Matrix& other) const;
+  void multiplyInplace(const Matrix& other);
+
+  Bra operator+(const Bra& other) const;
+  void operator+=(const Bra& other);
+
+  Bra operator-(const Bra& other) const;
+  void operator-=(const Bra& other);
+
+  bool operator==(const Bra& other) const;
+  bool operator!=(const Bra& other) const;
+};
