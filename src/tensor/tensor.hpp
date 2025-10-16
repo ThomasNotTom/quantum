6fbb7c659390b7b360225b62797a7dd8f3553281
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-10-16 15:03:00 +0100

    Create tensor class

---
+++ src/tensor/tensor.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+template <typename T> class Tensor {
+protected:
+  std::vector<T> values;
+
+public:
+  Tensor(unsigned int height);
+  Tensor(unsigned int height, const T initialValue);
+  Tensor(const std::vector<T>& values);
+
+  const T& get(unsigned int i) const;
+  T& get(unsigned int i);
+  void set(unsigned int i, T value) const;
+
+  unsigned int getRank() const;
+};
