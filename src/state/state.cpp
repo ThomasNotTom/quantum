304ca0bc0a170e15ff26a39e0f08faea7bdc6a08
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-12-02 01:17:40 +0000

    Create measure implementation

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -1,5 +1,7 @@
 #include "./state.hpp"
 
+std::mt19937 State::rng = std::mt19937(std::random_device{}());
+
 State::State(size_t basisCount) {
   for (size_t i = 0; i < basisCount; i++) {
     Vector basis = Vector(basisCount);
@@ -47,4 +49,22 @@ State State::normalise() const {
 
 float State::getProbability(size_t baseIndex) const {
   return this->bases[baseIndex].coefficient.modulusSquared();
+};
+
+size_t State::measure() {
+  std::vector<float> probabilities;
+
+  for (size_t i = 0; i < this->bases.size(); i++) {
+    probabilities.push_back(this->getProbability(i));
+  }
+
+  std::discrete_distribution<int> dist(probabilities.begin(),
+                                       probabilities.end());
+  size_t state = dist(State::rng);
+
+  for (size_t i = 0; i < this->bases.size(); i++) {
+    this->bases[i].coefficient = Complex(state == i ? 1 : 0, 0);
+  }
+
+  return state;
 };
