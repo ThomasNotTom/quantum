c3d05531fba5687e1e6a67fc0a46cec13ca95abe
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-12-02 01:10:58 +0000

    Create constructor implementation

---
+++ src/state/state.cpp
@@ -0,0 +1,10 @@
+#include "./state.hpp"
+
+State::State(size_t basisCount) {
+  for (size_t i = 0; i < basisCount; i++) {
+    Vector basis = Vector(basisCount);
+    basis.set(i, Complex(1, 0));
+
+    bases.push_back(BasisCoefficient(basis, Complex(0, 0)));
+  };
+}
